b4c1ad65c71f5424150243e5479a1bf677c54221
fallendamien <11881550+fallendamien@users.noreply.github.com>
2025-08-17 22:54:13 +0800

    Add DisplayName component and update main page
    Introduced a new DisplayName.svelte component with state and derived values, and imported it into +page.svelte. Also added a placeholder RandomNumber.svelte component and a VSCode settings file for editor configuration.

---
+++ src/lib/assets/components/DisplayName.svelte
@@ -0,0 +1,42 @@
+<script>
+	let firstName = $state('Zamri');
+	let lastName = $state('Rosli');
+	let fullName = $derived.by(() => {
+		console.log('fullName derived');
+		return `${firstName} ${lastName}`;
+	});
+	let username = $state('zamros');
+	let src = 'https://picsum.photos/250/300';
+	let bio = 'Hello, <b>this</b> is me.';
+
+	$effect(() => {
+		if (username || fullName) {
+			console.log('User has a username or a full name.');
+		}
+	});
+</script>
+
+<button
+	onclick={() => {
+		console.log(fullName);
+	}}>Get Full Name</button
+>
+<input bind:value={username} />
+<input
+	value={firstName}
+	oninput={(e) => {
+		firstName = e.currentTarget.value;
+		fullName = 'Jong';
+		console.log(fullName);
+	}}
+/>
+<input bind:value={lastName} />
+<!-- <img {src} alt="A photo of {name}" /> -->
+<h1>{username || fullName}</h1>
+<p>{@html bio}</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import DisplayName from '$lib/assets/components/DisplayName.svelte';
+</script>
+
+<h1>Outer</h1>
+<DisplayName />
